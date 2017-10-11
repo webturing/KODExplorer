ca9cd9582c86e83474095c449f86b71caeaf3fb9
luck <kalcaddle@foxmail.com>
2017-10-11 14:56:37 +0800

    解决压缩文件内文本文件预览不了的问题

--- app/controller/editor.class.php
+++ app/controller/editor.class.php
@@ -58,7 +58,7 @@ class editor extends Controller{
 					$displayName = rawurldecode($urlInfo['downFilename']);
 				}
 			}else{
-				if(is_file($filepath) || substr($filepath,0,4) != 'http'){
+				if(is_file($fileUrl) || substr($fileUrl,0,4) != 'http'){
 					show_json(LNG('not url'),false);
 				}
 				$displayName = rawurldecode($urlInfo['name']);
